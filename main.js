570a5519514965baa81a179e4e6b4345e9c8b2cc
Botmasher <languagejosh@hotmail.com>
2018-07-24 09:39:17 -0700

    Simplify guessing, format answer, refactor functional syntax

--- main.js
+++ main.js
@@ -9,90 +9,66 @@
  *     - try this way first
  */
 
-function storeAnswer(correctAnswer) {
-  return function(guessedAnswer) {
-    return (guessedAnswer == correctAnswer);
-  }
-}
+const storeAnswer = correctAnswer => (guessedAnswer => (guessedAnswer == correctAnswer));
 
-function linearHandleCheckAnswer(correctAnswer) {
+const linearHandleCheckAnswer = correctAnswer => {
   normalizedAnswer = correctAnswer.toLowerCase();
   return function(letter, i) {
     return (normalizedAnswer[i] == letter);
   }
-}
+};
 
-function guess(letter, checkHandler, i) {
+const guess = (letter, checkHandler, i) => {
   return (!badGuesses.includes(letter) && checkHandler(letter, i));
-}
+};
 
-function handleCheckAnswer(answer) {
-  return function(letter) {
-    return answer.split('').map((l, i) => {
-      if (letter === l) {
-        return i;
-      }
-    });
-  }
-}
+const handleCheckAnswer = answer => (letter => {
+  return answer.split('').map((l, i) => {
+    if (letter === l) {
+      return i;
+    }
+  });
+});
 
 // guess a letter regardless of index
-function guessManager(knownSlots, guessHandler) {
-  const guesses = {};
+const guessManager = (knownSlots, guessHandler) => {
   const possibleGuesses = 'abcdefghijklmnopqrstuvwxyz'
     .split('')
     .filter(guess => !knownSlots.includes(guess))
   ;
+  const fillAnswer = (letterId, letter) => {
+      knownSlots[letterId] = letter;
+  };
   // guess unknown letter
   possibleGuesses.map(letter => {
-    let letterIds = guessHandler(letter);
-    for (letterId of letterIds) {
-      if (letterId > -1) {
-        knownSlots[letterId] = letter;
-        guesses[letter] = true;
-        // finished filling out the answer
-        if (!knownSlots.includes('')) {
-          return knownSlots;
-        }
-      } else {
-        guesses[letter] = false;
-      }
-    }
+    guessHandler(letter).map(letterId => {
+      letterId > -1 && fillAnswer(letterId, letter);
+    })
   });
   return knownSlots;
-}
+};
 
-// guess index by index
-function guessManagerLinear(knownSlots, checkHandler) {
-  var badGuesses = {};
-  var possibleGuesses = 'abcdefghijklmnopqrstuvwxyz'.split();
-  for (var i=0; i < knownSlots.length; i++) {
-    // guess unknown letter
-    if (!knownSlots[i]) {
-      // TODO can this branch be unreached and leave 1+ empty slot?
-      for (var letter of possibleGuesses) {
-        if (guess(letter, checkHandler, i)) {
-          knownSlots[i] = letter;
-          break;
-        } else {
-          badGuesses[letter] = badGuesses.includes(letter) ? [...badGuesses[letter], i] : [i];
-        }
-      }
-    } else {
-      continue;   // correct letter known
-    }
-  }
-  return knownSlots;
-}
+const formatGuess = guessLetters => (
+  guessLetters
+    .map(letter => (letter ? letter : '?'))
+    .join('')
+);
 
 // TODO create puzzle generator
+// - have known answer display this managed by puzzle instead of guesser
 const puzzle = {
   answer: 'splash',
   slots: ['', '', '', 'a', '', '']
 };
 
-//const linearChecker = linearHandleCheckAnswer(puzzle.answer);
-//guessManagerLinear(puzzle.slots, linearChecker);
-
 const checker = handleCheckAnswer(puzzle.answer);
-console.log(guessManager(puzzle.slots, checker));
+const myGuess = guessManager(puzzle.slots, checker);
+
+// finished filling out the answer
+if (!myGuess.includes('')) {
+  console.log('Successfully guessed the word or phrase!');
+  console.log(`The answer is:  ${formatGuess(myGuess)}`);
+} else {
+  console.log('Guess was unsuccessful.');
+  console.log(`I only got as far as:  ${formatGuess(myGuess)}`);
+}
